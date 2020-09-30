f8fd288282a9ba3b910a93caed7dce4ecfb14db9
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2020-09-30 12:07:42 +0200

    Cleaning up the code and adding comments for the sound function

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -14,30 +14,28 @@ class ViewController: UIViewController {
     var player: AVAudioPlayer?
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-        if let buttonName = sender.titleLabel?.text {
-            playSound(buttonName: buttonName)
+        if let buttonName = sender.titleLabel?.text {  // Get the title of the button pressed
+            playSound(buttonName)  // Run the function of sound
         }
     }
     
-    func playSound(buttonName : String) {
-        guard let url = Bundle.main.url(forResource: buttonName, withExtension: "wav") else { return }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            /* iOS 10 and earlier require the following line:
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
+    // https://stackoverflow.com/questions/32036146/how-to-play-a-sound-using-swift
+    
+    //    func playSound(_ buttonName : String) {
+    //        guard let url = Bundle.main.url(forResource: buttonName, withExtension: "wav") else { return }
+    //        do {
+    //            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+    //            try AVAudioSession.sharedInstance().setActive(true)
+    //            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+    //            guard let player = player else { return }
+    //            player.play()
+    //        } catch let error {
+    //            print(error.localizedDescription)
+    //        }
+    
+    func playSound(_ buttonPressed : String) {
+        let url = Bundle.main.url(forResource: buttonPressed, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
     }
 }
