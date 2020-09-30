e75fc9e3002d1e98cca1005b28933dcaaf013fd0
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2020-09-30 11:58:34 +0200

    Added audio function

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,10 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
-        print(sender.titleLabel?.text! as Any)
+        if let buttonName = sender.titleLabel?.text {
+            playSound(buttonName: buttonName)
+        }
+    }
+    
+    func playSound(buttonName : String) {
+        guard let url = Bundle.main.url(forResource: buttonName, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
