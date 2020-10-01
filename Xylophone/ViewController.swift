5aa9e4ac0e5b26db695346b9bff5aafb42f8f512
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2020-10-01 08:37:32 +0200

    Finished app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,13 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
-import AVFoundation
+import AVFoundation  // To import the play sound library
 
 class ViewController: UIViewController {
     
-    var player: AVAudioPlayer?
+    var player: AVAudioPlayer?  // define the play sound
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-        if let buttonName = sender.titleLabel?.text {  // Get the title of the button pressed
-            playSound(buttonName)  // Run the function of sound
+        if let buttonTitle = sender.currentTitle {  // Get the title of the button pressed
+            playSound(buttonTitle)  // Run the function of sound using button title
             sender.alpha = 0.5  // To give an opacity to the clicked button
             Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { (Timer) in  // To return the button to its origin
                 sender.alpha = 1
@@ -24,11 +16,10 @@ class ViewController: UIViewController {
     }
     
     // https://stackoverflow.com/questions/32036146/how-to-play-a-sound-using-swift
-    
     //    func playSound(_ buttonName : String) {
     //        guard let url = Bundle.main.url(forResource: buttonName, withExtension: "wav") else { return }
     //        do {
-    //            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+    //            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)  // To make it run sound in silent mode
     //            try AVAudioSession.sharedInstance().setActive(true)
     //            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
     //            guard let player = player else { return }
@@ -37,8 +28,10 @@ class ViewController: UIViewController {
     //            print(error.localizedDescription)
     //        }
     
-    func playSound(_ buttonPressed : String) {
-        let url = Bundle.main.url(forResource: buttonPressed, withExtension: "wav")
+    // MARK: Playsound Function:
+    
+    func playSound(_ soundName : String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player!.play()
     }
